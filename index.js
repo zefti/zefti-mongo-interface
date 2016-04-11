4a060c3b5555323d38a43dada2379194d35b8be6
RYAN OGLE <ryanogle@RYANs-MacBook-Air-2.local>
2016-04-11 03:11:17 -0700

    stuff

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-
+var utils = require('zefti-utils');
+var resolve3Arguments = utils.resolve3Arguments;
+var resolve4Arguments = utils.resolve4Arguments;
 var Mongo = function(db){
   this.db = db;
   return this;
+};
+
+var operatorMap = {
+    '$del' : '$unset'
+  , '$inc' : '$inc'
+  , '$dec' : '$inc'
+  , '$push' : '$addToSet'
+  , '$pull' : '$pullAll'
+};
+
+var findOperatorMap = {
+    '$gt' : '$gt'
+  , '$gte' : '$gte'
+  , '$lt' : '$lt'
+  , '$lte' : '$lte'
+  , '$pull' : '$pullAll'
 };
 
 Mongo.prototype.count = function(){
   this.db.count.apply(this.db, arguments);
 };
 
+Mongo.prototype.info = function(cb){
+  this.db.stats(function(err, res){
+    return cb(err, res);
+  });
+};
+
 Mongo.prototype.create = function(){
-  var intArgs = mongo3Arguments(arguments);
+  var intArgs = resolve3Arguments(arguments);
   var cb = intArgs[intArgs.length-1];
   var hash = intArgs[0];
+  if (utils.isEmpty(hash)) return cb({errCode:'56f6e1abec4ad22804a064f2'});
   var interimCb = function(err, result){
     if(!err && result.result && result.result.ok) return cb(null, hash);
     return cb(err, result);
@@ -21,8 +46,13 @@ Mongo.prototype.create = function(){
 };
 
 Mongo.prototype.find = function(){
-  var intArgs = mongo4Arguments(arguments);
-  var cb = intArgs[intArgs.length-1];
+  var intArgs = resolve4Arguments(arguments);
+  var hash = intArgs[0];
+  var fieldMask = intArgs[1];
+  var options = intArgs[2];
+  var cb = intArgs[3];
+  var query = formatQuery(hash);
+  intArgs[0] = query;
   var interimCb = function(err, result){
     result.toArray(function(err, result2){
       cb(err, result2);
@@ -32,30 +62,57 @@ Mongo.prototype.find = function(){
   this.db.find.apply(this.db, intArgs);
 };
 
+function formatQuery(hash){
+  var query = {};
+  for (var key in hash) {
+    if (!findOperatorMap[key]) {
+      query[key] = hash[key];
+    } else {
+      for (var key2 in hash[key]) {
+        if (!query[key2]) {
+          query[key2] = {};
+        }
+        query[key2][key] = hash[key][key2];
+      }
+    }
+  }
+  return query;
+}
+
+
+Mongo.prototype.update = function(){
+  var intArgs = resolve4Arguments(arguments);
+  var hash = intArgs[0];
+  var update = intArgs[1];
+  var options = intArgs[2];
+  var cb = intArgs[3];
+  var update2 = formatUpdate(update);
+  var interimCb = function(err, result){
+    if (err || !result || !result.result || result.result.ok !== 1 || result.result.nModified < 1) return cb({errCode:'56b7104cb6a6115737f29ebf', err:err});
+    return cb(err, update);
+  };
+  intArgs[1] = update2;
+  intArgs[3] = interimCb;
+  //console.log(update2);
+  this.db.update.apply(this.db, intArgs);
+};
+
+
+
+Mongo.prototype.updateById = function(id, update, options, cb){
+  //TODO: allow $set and $unset combined
+  var intArgs = mongo4Arguments(arguments);
+  intArgs[1] = {$set:update};
+  if (typeof intArgs[0] === 'string') intArgs[0] = {_id:intArgs[0]};
+  this.db.update.apply(this.db, intArgs);
+};
+
 Mongo.prototype.findAndModify = function(hash, sort, update, options, cb){
   var intArgs = mongo5Arguments(arguments);
   this.db.findAndModify.apply(this.db, intArgs);
   //return cb(null, '1');
 };
 
-/*
-Mongo.prototype.findById = function(id, fieldMask, options, cb){
-  var intArgs = mongo4Arguments(arguments);
-  var cbOrig = intArgs[intArgs.length -1];
-  if (typeof intArgs[0] === 'string') intArgs[0] = {_id:intArgs[0]};
-
-  var interimCb = function(err, result){
-    if (!err && result.length > 1){
-      var err = 'findById found 2 results';
-    } else {
-      result = result[0];
-    }
-    cbOrig(err, result);
-  };
-  intArgs[intArgs.length -1] = interimCb
-  this.find.apply(this, intArgs);
-};
-*/
 
 Mongo.prototype.findById = function(id, fieldMask, options, cb){
   arguments[0] = {_id:id};
@@ -92,20 +149,6 @@ Mongo.prototype.upsert = function(hash, update, options, cb){
   this.db.update.apply(this.db, intArgs);
 };
 
-Mongo.prototype.update = function(hash, update, options, cb){
-  //TODO: allow $set and $unset combined
-  var intArgs = mongo4Arguments(arguments);
-  this.db.update.apply(this.db, intArgs);
-};
-
-Mongo.prototype.updateById = function(id, update, options, cb){
-  //TODO: allow $set and $unset combined
-  var intArgs = mongo4Arguments(arguments);
-  intArgs[1] = {$set:update};
-  if (typeof intArgs[0] === 'string') intArgs[0] = {_id:intArgs[0]};
-  this.db.update.apply(this.db, intArgs);
-};
-
 Mongo.prototype.remove = function(hash, options, cb){
   var intArgs = mongo3Arguments(arguments);
   this.db.remove.apply(this.db, intArgs);
@@ -130,6 +173,13 @@ Mongo.prototype.removeFieldsById = function(id, fields, options, cb){
   intArgs[1] = removeUpdate;
   if (typeof intArgs[0] === 'string') intArgs[0] = {_id:intArgs[0]};
   this.db.update.apply(this.db, intArgs);
+};
+
+//TODO: only expose this in dev environments
+Mongo.prototype.removeAll = function(cb){
+  this.db.remove({}, function(err, result){
+    if (cb) return cb(err, result)
+  });
 };
 
 Mongo.prototype.addToSet = function(hash, update, options, cb){
@@ -198,6 +248,46 @@ function mongoArguments(arguments, num){
     intArgs[i] = newArgs[i] || {};
   }
   return intArgs;
+}
+
+function formatUpdate(hash){
+  var finalHash = {};
+  for (var key in hash) {
+    var operator = operatorMap[key] || '$set';
+    if (!finalHash[operator]) finalHash[operator] = {};
+
+    if (operator === '$unset'){
+      hash[key].forEach(function(field){
+        finalHash[operator][field] = "";
+      });
+    }
+
+    if (operator === '$set') {
+      finalHash[operator][key] = hash[key];
+    }
+
+    if (operator === '$inc' || operator === '$dec') {
+      for (var key2 in hash[key]) {
+        if (key === '$inc') finalHash[operator][key2] = hash[key][key2];
+        if (key === '$dec') {
+          finalHash[operator][key2] = -hash[key][key2];
+        }
+      }
+    }
+
+    if (operator === '$addToSet') {
+      for (var key2 in hash[key]) {
+        if (!finalHash[operator][key2]) finalHash[operator][key2] = {'$each':hash[key][key2]};
+      }
+    }
+
+    if (operator === '$pullAll') {
+      for (var key2 in hash[key]) {
+        finalHash[operator][key2] = hash[key][key2];
+      }
+    }
+  }
+  return finalHash;
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,1 +1,32 @@
-{}
+var assert = require('assert');
+var async = require('async');
+var ZeftiMongoInterface = require('zefti-mongo-interface');
+var zeftiTest = require('zefti-test');
+var dbTests = zeftiTest.dbTests;
+var dependencies = zeftiTest.dependencies;
+
+/* databases */
+var mongoTestDb = dependencies.dataSources.mongoTest;
+var mongoTest = new ZeftiMongoInterface(mongoTestDb);
+
+
+dbTests.setup(mongoTest);
+dbTests.info(mongoTest);
+dbTests.createWithoutContent(mongoTest);
+//dbTests.createWithoutPrimaryKey(mongoTest);
+dbTests.createItem(mongoTest);
+dbTests.findById(mongoTest);
+dbTests.updateNew(mongoTest);
+//dbTests.updateOld(mongoTest);
+dbTests.removeField(mongoTest);
+dbTests.multiUpdate(mongoTest);
+dbTests.updateAndRemoveField(mongoTest);
+dbTests.updateAndMultiFieldDelete(mongoTest);
+dbTests.increment(mongoTest);
+dbTests.incrementNewAndExisting(mongoTest);
+dbTests.incrementAndDecrement(mongoTest);
+dbTests.setAddition(mongoTest);
+dbTests.setRemoval(mongoTest);
+dbTests.findByFieldEquality(mongoTest);
+dbTests.findByDualFieldEquality(mongoTest);
+dbTests.findWithGtAndLt(mongoTest);
